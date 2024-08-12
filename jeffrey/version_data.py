498c5a0f38823303e41e2e20f15ddbc322f5758c
jihoahn9303 <rediate03@gmail.com>
2024-08-12 11:39:32 +0900

    Add functionality to initialize DVC

--- jeffrey/version_data.py
+++ jeffrey/version_data.py
@@ -1,11 +1,12 @@
 from jeffrey.config_schemas.config_schema import Config
 from jeffrey.utils.config_utils import get_config
+from jeffrey.utils.data_utils import initialize_dvc
 
 
 @get_config(config_path="../configs", config_name="config")
-def entrypoint(config: Config) -> None:
-    print(config)
+def version_data(config: Config) -> None:
+    initialize_dvc()
 
 
 if __name__ == "__main__":
-    entrypoint()  # type: ignore
+    version_data()  # type: ignore
